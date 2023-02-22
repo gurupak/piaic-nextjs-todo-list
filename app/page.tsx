e3be9f81e69f25d39207d82dc86aa3703a410346
Irfan Ahmed <irfan.pak@gmail.com>
2023-02-23 01:18:46 +0500

    new data

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,142 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
+import Script from "next/script";
+import { useState } from "react";
+import TodoAddText from "./todo-add-textbox";
+import Todolist from "./todo-list";
+import { v4 } from "uuid";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [todoValue, setTodoValue] = useState("");
+
+  const [taskData, setTaskData] = useState([
+    { id: 1, task: "Buy groceries for next week", status: "pending" },
+    { id: 2, task: "Renew car insurance", status: "pending" },
+    { id: 3, task: "ReSign up for online coursee", status: "pending" },
+  ]);
+
+  const addTodoHandler = (e) => {
+    setTodoValue(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+
+    if (todoValue === "" || todoValue.length < 1) return;
+    const todoVal = v4();
+    const newTask = { id: todoVal, task: todoValue, status: "pending" };
+    // console.log(newTask);
+    // setTodoValue(newTask, ...taskData);
+    setTaskData([newTask, ...taskData]);
+    // console.log(todoValue);
+    setTodoValue("");
+  };
+
+  const deleteHandler = (e) => {
+    const id = e;
+    const todos = taskData.filter((obj) => {
+      return obj.id !== id;
+    });
+    setTaskData(todos);
+  };
+
+  const statusChangeHandler = (e, val) => {
+    console.log(val.target.innerHTML);
+    const id = e;
+    let newTodos = [];
+    taskData.map((obj) => {
+      let newTodo = { ...obj };
+      if (obj.id === id) {
+        newTodo = {
+          id,
+          task: obj.task,
+          status: obj.status === "pending" ? "finished" : "pending",
+        };
+        val.target.innerHTML = obj.status === "pending" ? "PENDING" : "FINISHED";
+        val.target.className =
+          obj.status === "pending" ? "btn btn-warning ms-1" : "btn btn-success ms-1";
+      }
+      newTodos.push(newTodo);
+    });
+    setTaskData(newTodos);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <section className="vh-100 {styles.main_syle}">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-lg-9 col-xl-7">
+              <div className="card rounded-3">
+                <div className="card-body p-4">
+                  <h4 className="text-center my-3 pb-3">To Do App</h4>
+
+                  <form
+                    className="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2"
+                    onSubmit={submitTodoHandler}>
+                    <TodoAddText
+                      addHandler={addTodoHandler}
+                      submitHandler={submitTodoHandler}
+                      value={todoValue}
+                    />
+
+                    <div className="col-12">
+                      <button
+                        type="submit"
+                        className="btn btn-warning"
+                        data-mdb-toggle="modal"
+                        data-mdb-target="#exampleModal">
+                        Get tasks
+                      </button>
+                    </div>
+                  </form>
+                  <Todolist
+                    tasks={taskData}
+                    deleteTaskHandler={deleteHandler}
+                    statusChangeHandler={statusChangeHandler}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Coming Soon
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-mdb-dismiss="modal"
+                aria-label="Close"></button>
+            </div>
+            <div className="modal-body">The fetching from server will come later.</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-mdb-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Script type="text/javascript" src="/js/mdb.min.js" strategy="lazyOnload" />
+    </>
+  );
 }
